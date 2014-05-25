d82289cc867ffe9b62f6a4533d5ad232f5c2f578
Tom Powell <zifnab@zifnab06.net>
2014-05-25 11:46:08 -0600

    Misc changes - fix imports, bypass mods in checking posts

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import praw
+import re
+import time
 
 import local_config as config
 from mongoengine import *
@@ -43,41 +45,42 @@ while True:
 
             tags = re.findall(r'\[([\w+\-]+)\]', post.title)
 
-            comment = ''
+            comment = 'Your submission has been removed because:'
 
             #Remove posts with invalid tags
             for tag in tags:
                 if not tag.lower() in config.APPROVED_TAGLIST:
-                    comment += '\n\nYour submission has been removed because your title has incorrect primary tags. Please read our [rules](http://www.reddit.com/r/mcservers/wiki/index) and create a new post with [valid primary tags](http://www.reddit.com/r/mcservers/wiki/index#wiki_.5Btags.5D_.26_.7Bbrackets.7D)'
+                    comment += '\n\n- your title has incorrect primary tags. Please read our [rules](http://www.reddit.com/r/mcservers/wiki/index) and create a new post with [valid primary tags](http://www.reddit.com/r/mcservers/wiki/index#wiki_.5Btags.5D_.26_.7Bbrackets.7D)'
                     break
 
             #Remove posts with no tags
             if tags is None:
-                comment += '\n\nYour submission does not have any primary tags. Please read our [rules](http://www.reddit.com/r/mcservers/wiki/index) and create a new post with [valid primary tags](http://www.reddit.com/r/mcservers/wiki/index#wiki_.5Btags.5D_.26_.7Bbrackets.7D)'
+                comment += '\n\n- it does not have any primary tags. Please read our [rules](http://www.reddit.com/r/mcservers/wiki/index) and create a new post with [valid primary tags](http://www.reddit.com/r/mcservers/wiki/index#wiki_.5Btags.5D_.26_.7Bbrackets.7D)'
 
             #Remove posts with <350 body
             if post.selftext and len(post.selftext) < 350:
-                comment += '\n\nThis submission has been removed because your post is shorter than 350 characters. Typically, this means that your post does not conform to our [description formatting](http://www.reddit.com/r/mcservers/wiki/index#wiki_description_example). Please reformat your post and [send the moderators a message](http://www.reddit.com/message/compose?to=%2Fr%2Fmcservers&subject=Spam+Filter&message=Please%20put%20link%20to%20post%20here:%0A%0AName%20of%20your%20server:%0A%0AAdditional%20comments:) to have them look over the post and approve it.'
+                comment += '\n\n- your post is shorter than 350 characters. Typically, this means that your post does not conform to our [description formatting](http://www.reddit.com/r/mcservers/wiki/index#wiki_description_example). Please reformat your post and [send the moderators a message](http://www.reddit.com/message/compose?to=%2Fr%2Fmcservers&subject=Spam+Filter&message=Please%20put%20link%20to%20post%20here:%0A%0AName%20of%20your%20server:%0A%0AAdditional%20comments:) to have them look over the post and approve it.'
 
             #Nuke offline servers
             offline = ['offline', 'cracked', 'hamachi']
-            if any(word in post.title for word in offline) or any (word in post.selftext for word in offline):
-                comment += '\n\nThis submission has been removed. Please do not post Hamachi, Cracked/Offline, or Test Servers here, post them in [MCTestServers](http://www.reddit.com/r/mctestservers).'
+            if any(word in post.title.lower() for word in offline) or any (word in post.selftext.lower() for word in offline):
+                comment += '\n\n- Hamachi, Cracked, Offline, or test servers are not allowed here here, please post them in [MCTestServers](http://www.reddit.com/r/mctestservers).'
 
             #nuke 24/7
             tfs = ['24/7', '24x7']
-            if any (word in post.title for word in tfs) or any (word in post.selftext for word in tfs):
-                comment += "\n\nThis submission has been removed because it is labeled as 24/7. All servers are required to be online 24/7 to be posted to /r/mcservers. Please remove any reference to your server being 24/7 and repost."
+            if any (word in post.title.lower() for word in tfs) or any (word in post.selftext.lower() for word in tfs):
+                comment += "\n\n- it is labeled as 24/7. All servers are required to be online 24/7 to be posted to /r/mcservers. Please remove any reference to your server being 24/7 and repost."
 
             #Remove post if any of above tests passed
-            if comment != '':
+            if comment != 'Your submission has been removed because:':
                 comment +='\n\n*I am a bot, this action was performed automatically. If you feel this was a mistake, please [message the moderators](http://reddit.com/message/compose?to=/r/mcservers&subject={0})*'.format(post.url)
-                c = post.add_comment(comment[2:])
+                c = post.add_comment(comment)
                 c.distinguish()
                 post.remove()
                 print('Post was removed tags: {0} - {1} - {2}'.format(post.id, strip_utf8(post.title), post.url))
             Submission(i=post.id, u=post.author.name, t=post.title, s=post.selftext, d=datetime.utcnow()).save()
             already_done.append(post.id)
         time.sleep(5)
-    except:
+    except Exception as e:
+        print (e)
         pass
