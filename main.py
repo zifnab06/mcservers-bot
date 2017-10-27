4b32728a068f476083a1e876414206f48c471707
Tom Powell <zifnab@zifnab06.net>
2017-10-27 03:30:44 +0000

    update praw to 5.2.0

--- main.py
+++ main.py
@@ -25,33 +25,28 @@ connect('mcservers')
 
 slack.api_token = config.SLACK_API_TOKEN
 
-r = praw.Reddit(user_agent=u"/u/mcservers-bot v1.1")
+r = praw.Reddit(client_id=config.CLIENT_ID, client_secret=config.CLIENT_SECRET, password=config.PASSWORD, username=config.USERNAME, user_agent='/u/mcservers-bot v1.2')
 
-r.login(config.USERNAME, config.PASSWORD)
-
-moderators = [x.name for x in r.get_subreddit('mcservers').get_moderators()]
+moderators = [x.name for x in r.subreddit('mcservers').moderator()]
 slack.chat.post_message(u'#bot-log', u'Bot Started', username=config.USERNAME)
 
-#needs to be moved to a wiki config
-with open('reasons.yml') as f:
-    reasons = yaml.load(f.read())
 
 print(u'{0} posts in database'.format(len(Submission.objects())))
 
 #Main loop. Loop forever, do things in a try/catch for when reddit dies. 
-while True:
+for post in r.subreddit('mcservers').stream.submissions():
     try: 
-        for post in r.get_subreddit(config.SUBREDDIT).get_new(limit=10):
-            #TODO: claen this up. already_done used to contain a list of all posts, but bot was using 100% mem after a year
-            already_done = Submission.objects(i=post.id).first()
-            if already_done is not None:
-                continue
-            chat_message = u'{0} - {1} \n user: {2}'.format(post.title, post.url, post.author.name)
-            slack.chat.post_message(u'#mcservers-feed', chat_message, username=config.USERNAME)
-
-            Submission(i=post.id, u=post.author.name, t=post.title, s=post.selftext, d=datetime.utcnow(), r=bool(remove)).save()
-        #Sleep for 30 seconds, current max rate being 10 posts / 30 seconds
-        time.sleep(30)
+        #TODO: claen this up. already_done used to contain a list of all posts, but bot was using 100% mem after a year
+        already_done = Submission.objects(i=post.id).first()
+        if already_done is not None:
+            continue
+        chat_message = u'{0} - {1} \n user: {2}'.format(post.title, post.url, post.author.name)
+        #find last submission by user
+        last = Submission.objects(u=post.author.name).order_by("-d").first()
+        if last:
+            chat_message += " last post: https://redd.it/{} ({} days ago)".format(last.i, (datetime.utcnow()-last.d).days)
+        slack.chat.post_message(u'#mcservers-feed', chat_message, username=config.USERNAME)
+        Submission(i=post.id, u=post.author.name, t=post.title, s=post.selftext, d=datetime.utcnow(), r=bool(False)).save()
 
     except Exception as e:
         '''We do nothing on an error but print the error - keeps bot from dying when reddit goes down'''
